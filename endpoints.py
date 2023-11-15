a36bf30fcfbf535a76644311c13050e6d0a5c6de
apecuch <apecuch@fi.uba.ar>
2023-11-14 21:15:06 -0300

    Modifico endpoints.py

--- endpoints.py
+++ endpoints.py
@@ -1,10 +1,12 @@
 import requests
 
-TOKEN:str = '' # completar con el token de la API
-AUTHORIZATION:str = 'Bearer ' + TOKEN
+
+TOKEN:str = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.DGI_v9bwNm_kSrC-CQSb3dBFzxOlrtBDHcEGXvCFqgU'
+AUTH:dict = {'Authorization': f'Bearer {TOKEN}'}
 URL:str = 'http://vps-3701198-x.dattaweb.com:4000'
 
-def get_cinemas(token:str) -> dict:
+
+def get_cinemas() -> list[dict]:
     
     '''
     Obtener información de todos los cines.
@@ -13,17 +15,17 @@ def get_cinemas(token:str) -> dict:
     endpoint:str = f'{URL}/cinemas'
     
     try:
-        response = requests.get(endpoint, headers={'Authorization': AUTHORIZATION })
+        response = requests.get(endpoint, headers = AUTH)
         response.raise_for_status()
         
-        cinemas:dict = response.json()
+        cinemas:list[dict] = response.json()
         return cinemas
     
     except requests.exceptions.HTTPError as err:
         raise SystemExit('ERROR: ' + str(err))
 
 
-def get_movies_by_cinema(token:str, cinema_id:int) -> dict:
+def get_movies_by_cinema(cinema_id:int) -> list[dict]:
     
     '''
     Obtener películas proyectadas en un determinado cine.
@@ -32,10 +34,10 @@ def get_movies_by_cinema(token:str, cinema_id:int) -> dict:
     endpoint:str = f'{URL}/cinemas/{cinema_id}/movies'
     
     try:
-        response = requests.get(endpoint, headers={'Authorization': AUTHORIZATION })
+        response = requests.get(endpoint, headers = AUTH)
         response.raise_for_status()
     
-        movies_by_cinema:dict = response.json()
+        movies_by_cinema:list[dict] = response.json()
         return movies_by_cinema
     
     except requests.exceptions.HTTPError as err:
