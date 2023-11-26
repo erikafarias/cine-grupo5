f7a25e6e42539fd57d4685cdfe4a977894684b6f
apecuch <apecuch@fi.uba.ar>
2023-11-26 15:48:00 -0300

    Arreglo para usar el nuevo dict

--- main.py
+++ main.py
@@ -4,33 +4,33 @@ import endpoints
 from utils import decodificar_imagen_base64
 
 
-def mostrar_pelicula(pelicula:dict, window:tk, movie_poster:str) -> None:
+def mostrar_pelicula(movie:dict, window:tk, movie_poster:str) -> None:
     
     poster = ImageTk.PhotoImage(decodificar_imagen_base64(movie_poster))
     poster_label = tk.Label(window, image=poster, bg='black')
     poster_label.pack(pady = 10)
     
-    nombre_pelicula_texto = pelicula['name']
+    nombre_pelicula_texto = movie['name']
     nombre_pelicula = tk.Label(window, text = nombre_pelicula_texto, font = ('Calibri', 18, "bold"), bg='#2B2A33', fg = '#FFFFFF', anchor='center')
     nombre_pelicula.pack(pady = (30,10))
     
-    sinopsis_texto = pelicula['synopsis']
+    sinopsis_texto = movie['synopsis']
     sinopsis = tk.Label(window, text = sinopsis_texto, font = ('Calibri', 10, 'italic'), bg='#2B2A33', fg = '#FFFFFF', wraplength = 600, justify= tk.CENTER)
     sinopsis.pack(pady = 5)
     
-    duracion_texto = pelicula['duration']
+    duracion_texto = movie['duration']
     duracion = tk.Label(window, text = duracion_texto, font = ('Calibri', 10), bg='#2B2A33', fg = '#FFFFFF', anchor='center')
     duracion.pack(pady = 5)
     
-    actores_texto = pelicula['actors']
+    actores_texto = movie['actors']
     actores = tk.Label(window, text = actores_texto, font = ('Calibri', 10), bg='#2B2A33', fg = '#FFFFFF', anchor='center')
     actores.pack(pady = 5)
     
-    genero_texto = pelicula['gender']
+    genero_texto = movie['gender']
     genero = tk.Label(window, text = genero_texto, font = ('Calibri', 10, 'bold'), bg='#2B2A33', fg = '#FFFFFF', anchor='center')
     genero.pack(pady = 5)
     
-    rating_texto = pelicula['rating']
+    rating_texto = movie['rating']
     rating = tk.Label(window, text = rating_texto, font = ('Calibri', 10, 'bold'), bg='#2B2A33', fg = '#FFFFFF', anchor='center')
     rating.pack(pady = 5)
 
@@ -62,7 +62,11 @@ def mostrar_sala(cinema:dict, window:tk) -> None:
         label.pack(pady = 10)
 
 
-def pantalla_secundaria(cinema:dict, movie:dict, movie_poster:str) -> None:
+def pantalla_secundaria(ID_cinema:str, ID_pelicula:str) -> None:
+    
+    cinema:dict = endpoints.get_cinema_info_by_id(ID_cinema)
+    movie:dict = endpoints.get_movie_by_id(ID_pelicula)
+    movie_poster:str = endpoints.get_poster_by_id(movie['poster_id'])['poster_image']
     
     window = tk.Tk(screenName='Pantalla Secundaria')
     window.geometry("1280x720")
@@ -97,8 +101,22 @@ def pantalla_secundaria(cinema:dict, movie:dict, movie_poster:str) -> None:
     window.mainloop()
 
 
-movie:dict = endpoints.get_movie_by_id('3')
-movie_poster:str = endpoints.get_movie_poster('3')
-cinema:dict = endpoints.get_cinema_info_by_id('3')
+def main() -> None:
+    
+    # Diccionario que guarda toda la info correspondiente a la compra ha realizar. Se utiliza en todas las ventanas.
+    compra: dict = {
+        'ID_QR'             : '', # Este arrancatia vacio y se llena una vez realizada la compra y generado el QR
+        'ID_pelicula'       : '2', # Pelicula elegida por el usuario en la pantalla principal
+        'nombre_pelicula'   : '',
+        'ID_cinema'         : '2', # Cine elegido por el usuario en la pantalla principal
+        'cantidad_entradas' : 0,
+        'precio_entrada'    : 0, # Este ya lo definimos aca hardcodeado
+        'snacks'            : [ ['nombre snack', 'cantidad', 'precio unitario'] , ["nombre snack", 'cantidad', 'precio unitario'] ],
+        'timestamp_compra'  : '' # Hora de la compra -> Cuando el usuario selecciona "comprar" en la pantalla checkout
+    }
 
-pantalla_secundaria(cinema, movie, movie_poster)
+    pantalla_secundaria(compra['ID_cinema'], compra['ID_pelicula'])
+
+main()
+
+
